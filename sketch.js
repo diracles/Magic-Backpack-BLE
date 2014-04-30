88ba652916746a4f215db4154a82b1ced0f74e15
ALEXANDRA_DIRACLES <A.DIRACLES@GMAIL.COM>
2014-04-29 20:39:19 -0400

    ant over backdrop

--- sketch.js
+++ sketch.js
@@ -3,25 +3,35 @@
 // http://natureofcode.com
 
 var mover;
+var mySound = new buzz.sound('rhodes_loop.wav');
+var canvas;
+var img2;
+var backdrop;
 
 function setup() {
-  createCanvas(640,360);
+  canvas = createCanvas(1231,748);
   mover = new Mover(); 
-  img =loadImage("img/desert.png");
+  backdrop =loadImage("img/desert.png");
   img2 = loadImage("img/ant.png");
-  console.log("img2");
+  console.log("img2 in setup");
+  canvas.position(0,0);
 }
 
 function draw() {
   // background(51);
-  pushMatrix();
-  imageMode(CENTER);
-  image(img2, 360, 240);
-  popMatrix();
 
-  
+  imageMode(CENTER);
+  image(backdrop, 400, 400, width, height);
+  image(img2, 1231, 748);
+    //somehow changing this got rid of 2nd ant eater
   mover.update();
   mover.checkEdges();
   mover.display(); 
+}
+
+
+function mousePressed() {
+  // Using the third-party library to call play() on the buzz object
+  mySound.play();
 }
 
